9646fd414c3e7da4282e603088c4f2b290646887
faysal-alam-sazim <faysal.alam@sazim.io>
2024-08-27 15:01:43 +0600

    feat(ST-46): add get classrooms api for teacher

--- src/classrooms/classrooms.repository.ts
+++ src/classrooms/classrooms.repository.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@nestjs/common";
 import { EntityRepository } from "@mikro-orm/postgresql";
 
 import { Classroom } from "@/common/entities/classrooms.entity";
+import { Teacher } from "@/common/entities/teachers.entity";
 
 @Injectable()
 export class ClassroomsRepository extends EntityRepository<Classroom> {}
