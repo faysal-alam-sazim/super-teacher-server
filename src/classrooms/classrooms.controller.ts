1747977f70dede6a0e7367496fabd9c0139f698e
faysal-alam-sazim <faysal.alam@sazim.io>
2024-09-10 14:19:01 +0600

    feat(ST-63): add remove student from classroom api (#29)

--- src/classrooms/classrooms.controller.ts
+++ src/classrooms/classrooms.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Post, UseGuards, UseInterceptors } from "@nestjs/common";
 
 import { ITokenizedUser } from "@/auth/auth.interfaces";
 import { CurrentUser } from "@/auth/decorators/current-user.decorator";
@@ -6,10 +6,12 @@ import { Roles } from "@/auth/decorators/roles.decorator";
 import { JwtAuthGuard } from "@/auth/guards/jwt-auth.guard";
 import { RolesGuard } from "@/auth/guards/roles.guard";
 import { EUserRole } from "@/common/enums/roles.enum";
+import { ResponseTransformInterceptor } from "@/common/interceptors/response-transform.interceptor";
 
 import { CreateClassroomDto, EnrollStudentDto } from "./classrooms.dto";
 import { ClassroomsService } from "./classrooms.service";
 
+@UseInterceptors(ResponseTransformInterceptor)
 @UseGuards(JwtAuthGuard)
 @Controller("classrooms")
 export class ClassroomsController {
@@ -27,10 +29,20 @@ export class ClassroomsController {
     return this.classroomsService.createClassroom(classroomDto, user.id);
   }
 
-  @Post("enroll")
+  @Post("students")
   @UseGuards(RolesGuard)
   @Roles(EUserRole.TEACHER)
   addStudent(@Body() enrollStudentDto: EnrollStudentDto) {
     return this.classroomsService.addStudent(enrollStudentDto);
+  }
+
+  @Delete("students")
+  @UseGuards(RolesGuard)
+  @Roles(EUserRole.TEACHER)
+  removeStudentFromClassroom(
+    @CurrentUser() user: ITokenizedUser,
+    @Body() deleteEnrollDto: EnrollStudentDto,
+  ) {
+    return this.classroomsService.removeStudentFromClassroom(user.id, deleteEnrollDto);
   }
 }
