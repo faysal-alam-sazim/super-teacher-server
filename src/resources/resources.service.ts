5e002b76c6de619a142207e3556193c4be9ad34d
Faysal Alam <faysal.alam@sazim.io>
2024-09-20 17:06:20 +0600

    feat(104): add file upload handler inside file service

--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from "@nestjs/common";
 
-import { EntityManager } from "@mikro-orm/postgresql";
-
-import { randomUUID } from "crypto";
-
 import { ClassroomsRepository } from "@/classrooms/classrooms.repository";
-import { PresignedUrlFile } from "@/file-uploads/file-uploads.dtos";
 import { FileUploadsService } from "@/file-uploads/file-uploads.service";
 
 import { AddResourcesDto } from "./resources.dtos";
@@ -17,30 +12,23 @@ export class ResourcesService {
     private readonly resourcesRepository: ResourcesRepository,
     private readonly fileUploadsService: FileUploadsService,
     private readonly classroomsRepository: ClassroomsRepository,
-    private readonly em: EntityManager,
   ) {}
 
-  addResources(classroomId: number, addResourcesDto: AddResourcesDto, file: Express.Multer.File) {
-    this.em.transactional(async (em) => {
-      const classroom = await this.classroomsRepository.findOneOrFail({ id: classroomId });
+  async addResources(
+    classroomId: number,
+    addResourcesDto: AddResourcesDto,
+    file: Express.Multer.File,
+  ) {
+    const classroom = await this.classroomsRepository.findOneOrFail({ id: classroomId });
 
-      const presignedUrlFile = new PresignedUrlFile();
-      presignedUrlFile.name = `${randomUUID()}-${new Date().getTime()}-${file.originalname}`;
-      presignedUrlFile.type = file.mimetype;
+    const fileUrl = await this.fileUploadsService.handleFileUpload(file);
 
-      const { name, signedUrl } = await this.fileUploadsService.getPresignedUrl(presignedUrlFile);
-
-      await this.fileUploadsService.uploadToS3(signedUrl, file);
-
-      const fileUrl = `${process.env.AWS_S3_ENDPOINT}/${process.env.AWS_S3_BUCKET_NAME}//${name}`;
-
-      const resource = this.resourcesRepository.create({
-        ...addResourcesDto,
-        fileUrl,
-        classroom: classroom.id,
-      });
-
-      await em.persistAndFlush(resource);
+    const resource = this.resourcesRepository.create({
+      ...addResourcesDto,
+      fileUrl,
+      classroom: classroom.id,
     });
+
+    await this.resourcesRepository.getEntityManager().persistAndFlush(resource);
   }
 }
