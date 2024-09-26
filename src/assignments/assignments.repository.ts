58147714f1bc0c6d8fc5e94766d37d4add897626
faysal-alam-sazim <faysal.alam@sazim.io>
2024-09-26 10:57:28 +0600

    feat(ST-110): add update assignment api (#68)

--- src/assignments/assignments.repository.ts
+++ src/assignments/assignments.repository.ts
@@ -4,5 +4,15 @@ import { EntityRepository } from "@mikro-orm/postgresql";
 
 import { Assignment } from "@/common/entities/assignments.entity";
 
+import { UpdateAssignmentDto } from "./assignments.dtos";
+
 @Injectable()
-export class AssignmentsRepository extends EntityRepository<Assignment> {}
+export class AssignmentsRepository extends EntityRepository<Assignment> {
+  async updateOne(assignmet: Assignment, assignmentToUpdate: UpdateAssignmentDto) {
+    this.assign(assignmet, assignmentToUpdate);
+
+    await this.em.persistAndFlush(assignmet);
+
+    return assignmet;
+  }
+}
