e6a1781769134caff516202e70b5355d9b42925a
faysal-alam-sazim <faysal.alam@sazim.io>
2024-10-09 21:47:12 +0600

    feat(ST-149): add verify otp endpoint (#90)

--- src/verify-users/verify-users.controller.ts
+++ src/verify-users/verify-users.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, UseInterceptors } from "@nestjs/common";
 
 import { ResponseTransformInterceptor } from "@/common/interceptors/response-transform.interceptor";
 
-import { VerifyEmailDto } from "./verify-users.dtos";
+import { VerifyEmailDto, VerifyOtpDto } from "./verify-users.dtos";
 import { VerifyUsersService } from "./verify-users.service";
 
 @UseInterceptors(ResponseTransformInterceptor)
@@ -13,5 +13,10 @@ export class VerifyUsersController {
   @Post("email")
   verifyEmail(@Body() verifyEMailDto: VerifyEmailDto) {
     this.verifyUsersService.verifyUserAndSendOtp(verifyEMailDto.email);
+  }
+
+  @Post("otp")
+  verifyOtp(@Body() verifyOtpDto: VerifyOtpDto) {
+    return this.verifyUsersService.verifyOtp(verifyOtpDto.email, verifyOtpDto.otp);
   }
 }
