d7c4d008683f37269ce75f4338f0aec9e23bb716
Faysal Alam <faysal.alam@sazim.io>
2024-10-16 20:32:25 +0600

    feat(ST-158): add e2e test for get users/students api

--- test/utils/helpers/users.helpers.ts
+++ test/utils/helpers/users.helpers.ts
@@ -3,9 +3,10 @@ import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import * as argon2 from "argon2";
 
 import { ARGON2_OPTIONS } from "@/common/config/argon2.config";
+import { EUserRole } from "@/common/enums/roles.enum";
 
 import { MOCK_AUTH_EMAIL, MOCK_AUTH_PASS } from "../../auth/auth.mock";
-import { UserFactory } from "../factories/users.factory";
+import { StudentsFactory, UserFactory } from "../factories/users.factory";
 
 export const createUserInDb = async (
   dbService: EntityManager<IDatabaseDriver<Connection>>,
@@ -36,4 +37,24 @@ export const createUserInDb = async (
   await dbService.flush();
 
   return user;
+};
+
+export const createStudentUsersInDb = async (
+  dbService: EntityManager<IDatabaseDriver<Connection>>,
+  numberOfUsers = 1,
+) => {
+  const userFactory = new UserFactory(dbService);
+  const users = userFactory.make(numberOfUsers).map((user) => {
+    user.role = EUserRole.STUDENT;
+    return user;
+  });
+
+  const studentFactory = new StudentsFactory(dbService);
+  const students = users.map((user) => {
+    const student = studentFactory.makeOne();
+    student.user = user;
+    return student;
+  });
+
+  await dbService.persistAndFlush([...users, ...students]);
 };
